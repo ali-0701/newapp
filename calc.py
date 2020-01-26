7bca75acfd3ace31c96c3c6d84a32e5fcdcae897
Abdelrahman Ali <abdelrahman.sa.ali@gmail.com>
2020-01-26 15:30:56 -0700

    add main function

--- calc.py
+++ calc.py
@@ -9,3 +9,18 @@ def sum(x,y):
 
 def sub(x,y):
     return x-y
+
+def main():
+    x = 10.0
+    y = 5.0
+    # to add
+    print("%f + %f = %f" % (x, y, sum(x,y)))
+
+    # to subtract
+    print("%f - %f = %f" % (x, y, sub(x,y)))
+
+    # to add
+    print("%f * %f = %f" % (x, y, multiply(x,y)))
+
+    # to add
+    print("%f / %f = %f" % (x, y, divide(x,y)))
